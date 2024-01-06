a3719206aa5fe2413e0cd286d90db0c4ca5e4033
Emanuel Cantú Rodríguez <emanuelcantu.835@gmail.com>
2024-01-05 19:59:47 -0600

    Primer Commit

---
+++ src/main/java/com/example/ejemploIndat2/EjemploIndat2Application.java
@@ -0,0 +1,13 @@
+package com.example.ejemploIndat2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemploIndat2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjemploIndat2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ejemploIndat2/EjemploIndat2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ejemploIndat2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemploIndat2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
